89b2d48e704485bb6d9797852a6fe411c2b0994a
luson <ljhxai@163.com>
2016-05-11 16:22:43 +0800

    up to v0.1.7

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -3,12 +3,12 @@ var oss = require('../index');
 
 gulp.task('upload_to_oss', function(){
     var options = {
-        accessKeyId: '6fVgt8d5fGWgESXz',
-        secretAccessKey: '80KOHp7pXGP3diOcRix5ITMeyeJo6F',
+        accessKeyId: 'xxxxxxxxx',
+        secretAccessKey: 'xxxxxxx',
         // endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
         endpoint: 'http://oss-cn-hangzhou.aliyuncs.com',
         apiVersion: '2013-10-15',
-        prefix: 'plantUnion/',
+        prefix: 'xxxxxxx',
         bucket: 'qhcdn'
     };
     return gulp.src(['./js/**/*']).pipe(oss(options));
